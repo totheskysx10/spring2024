c18bbd7038a5cc4eb0b3b0531f5549df007ae766
Totheskysx10 <crazycat87654@gmail.com>
2024-02-13 22:25:24 +0500

    initial

---
+++ src/test/java/com/spring/vsurin/bookexchange/BookExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vsurin.bookexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
